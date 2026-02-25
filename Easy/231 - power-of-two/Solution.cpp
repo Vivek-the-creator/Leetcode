9588c48af6cbaa18152382e7dc16069f7838e51c
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-02-25 11:28:52 +0530

    Sync LeetCode submission - Power Of Two

---
+++ Easy/231 - power-of-two/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        int k = n & (n-1);
+        
+        if(k == 0){
+            return true;
+        }
+        
+        return false;
+    }
+   
+};
