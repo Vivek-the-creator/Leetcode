5e2000d8d317f8d2d8ebfa7878281138b4734fc3
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-03-12 09:39:25 +0530

    Sync LeetCode submission - Find Peak Element

---
+++ Medium/162 - find-peak-element/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(mid - 1 >= 0 && mid + 1 < nums.size() && nums[mid - 1] < nums[mid] && nums[mid + 1] < nums[mid]) return mid;
+            if(nums[mid] < nums[mid+1]){
+                start = mid+1;
+            }
+            else end = mid;
+        }
+
+        return start;
+        
+    }
+};
