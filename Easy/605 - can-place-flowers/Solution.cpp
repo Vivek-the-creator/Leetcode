a5b117df1de78d04222c1be01f33c8ffb37193be
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-03-14 09:20:46 +0530

    Sync LeetCode submission - Can Place Flowers

---
+++ Easy/605 - can-place-flowers/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count=0;
+        if(flowerbed.size() == 1 && flowerbed[0] == 0){
+            count++;
+        }
+        if(flowerbed.size() == 1 && flowerbed[0] == 1){
+            count = 0;
+        }
+        if(flowerbed.size() > 1 && flowerbed[0] == 0 && flowerbed[1] == 0){
+            count++;
+            flowerbed[0] = 1;
+        }
+        
+        for(int i=1; i<flowerbed.size()-1; i++){
+            if(flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
+                count++;
+                flowerbed[i] = 1;
+            }
+        }
+        if(flowerbed.size() > 1 && flowerbed[flowerbed.size()-1] == 0 && flowerbed[flowerbed.size()-2] == 0){
+            count++;
+            flowerbed[flowerbed.size()-1] = 1;
+        }
+     
+        if(count >= n){
+            return true;
+        }
+        return false;
+    }
+};
