b855dab9d329fb9589a7c2ce31b63c1f2f9a2390
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-03-16 18:58:37 +0530

    Sync LeetCode submission - Unique Number Of Occurrences

---
+++ Easy/1319 - unique-number-of-occurrences/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int, int>freq;
+        for(int i=0; i<arr.size(); i++){
+            freq[arr[i]]++;
+        }
+        vector<int>ans;
+        for(auto it:freq){
+            ans.push_back(it.second);
+        }
+        sort(ans.begin(), ans.end());
+        for(int i=0; i<ans.size()-1; i++){
+            if(ans[i]==ans[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
