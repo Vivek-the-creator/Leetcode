76a40ec2422918e96963e443c2f421f84ce41e2b
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-06-08 19:58:09 +0530

    Sync LeetCode submission - Longest Consecutive Sequence

---
+++ Medium/128 - longest-consecutive-sequence/Solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if(nums.size() == 0){
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+
+        int count = 1;
+        int max_count = 1;
+
+        for(int i = 1; i < nums.size(); i++) {
+
+            if(nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if(nums[i] == nums[i - 1] + 1) {
+                count++;
+            }
+            else {
+                max_count = max(max_count, count);
+                count = 1;
+            }
+        }
+
+        max_count = max(max_count, count);
+
+        return max_count;
+    }
+};
