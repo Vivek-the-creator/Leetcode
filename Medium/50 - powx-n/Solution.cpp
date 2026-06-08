bd7fabaf0af319f700a0eec5c50314384e34d99d
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-06-08 22:35:20 +0530

    Sync LeetCode submission - Powx N

---
+++ Medium/50 - powx-n/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0 || x==1){
+            return 1;
+        }
+        long long num = n;
+        if(num < 0){
+            x = 1/x;
+            num = -num;
+        }
+        double ans = 1;
+        while(num>0){
+            if(num%2 == 1){
+                ans*=x;
+
+            }
+            x *= x;
+            num = num/2;
+        }
+        return ans;
+    }
+};
