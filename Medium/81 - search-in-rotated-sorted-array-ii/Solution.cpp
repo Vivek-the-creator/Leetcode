5746ed220f62ba59258b8c0961565e2d1566b16e
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-07-16 18:22:46 +0530

    Sync LeetCode submission - Search In Rotated Sorted Array Ii

---
+++ Medium/81 - search-in-rotated-sorted-array-ii/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        int left = 0;
+        int right = nums.size()-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            else if(nums[mid] <= nums[right]){
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                } else{
+                    right = mid-1;
+                }
+            } else{
+                if(target <= nums[mid] && target >= nums[left]){
+                    right = mid-1;
+                } else{
+                    left = mid+1;
+                }
+            }
+
+        }
+        return false;
+    }
+};
