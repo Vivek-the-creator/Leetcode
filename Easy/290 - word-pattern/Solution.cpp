893fc3abf30e8462a8048bea6aa2772d19e00994
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-09-07 21:47:34 +0530

    Sync LeetCode submission - Word Pattern

---
+++ Easy/290 - word-pattern/Solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string word;
+        stringstream ss(s);
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        if (pattern.length() != words.size())
+            return false;
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern[i];
+            string w = words[i];
+            if (charToWord.count(c)) {
+                if (charToWord[c] != w)
+                    return false;
+            }
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != c)
+                    return false;
+            }
+            charToWord[c] = w;
+            wordToChar[w] = c;
+        }
+        return true;
+    }
+};
