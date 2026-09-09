315c57b8ceeaf61a9deacd8cf2956ada38eef9cf
Vivek-the-creator <vivek.kk2024aiml@sece.ac.in>
2026-09-09 21:26:02 +0530

    Sync LeetCode submission - Subarrays With K Different Integers

---
+++ Hard/1034 - subarrays-with-k-different-integers/Solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int atMost(vector<int>& nums, int k) {
+
+        if (k == 0)
+            return 0;
+
+        unordered_map<int, int> freq;
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            // Add current element
+            freq[nums[right]]++;
+
+            // Too many distinct elements
+            while (freq.size() > k) {
+
+                freq[nums[left]]--;
+
+                if (freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+
+                left++;
+            }
+
+            // Number of valid subarrays ending at right
+            ans += right - left + 1;
+        }
+
+        return ans;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+};
