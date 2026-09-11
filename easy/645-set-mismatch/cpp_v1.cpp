cd08f30b238fa23f29ee8c050c778a4e0df267ed
Vivek K K <vivek.kk2024aiml@sece.ac.in>
2026-09-11 09:22:22 +0530

    Add solution: 645. Set Mismatch (cpp, v1)

---
+++ easy/645-set-mismatch/cpp_v1.cpp
@@ -0,0 +1,27 @@
+// Pushed: 2026-09-11 03:51:24 UTC
+// Difficulty: Easy
+// Runtime: 0 ms
+// Memory: 25.2 MB
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int duplicate = -1, missing = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                duplicate = abs(nums[i]);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        return {duplicate, missing};
+    }
+};
