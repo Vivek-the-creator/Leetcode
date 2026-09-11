2912f7e21251f51696373d876144dae2cf7d2995
Vivek K K <vivek.kk2024aiml@sece.ac.in>
2026-09-11 09:19:30 +0530

    Add solution: 485. Max Consecutive Ones (cpp, v1)

---
+++ easy/485-max-consecutive-ones/cpp_v1.cpp
@@ -0,0 +1,21 @@
+// Pushed: 2026-09-11 03:48:32 UTC
+// Difficulty: Easy
+// Runtime: 5 ms
+// Memory: 50.2 MB
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi = 0;
+        int count = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                count++;
+                maxi = max(maxi, count);
+            }else{
+                count = 0;
+            }
+        }
+        return maxi;
+    }
+};
