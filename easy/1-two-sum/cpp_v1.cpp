a07e4f9dd3bc51470c3348588526213f8e3749fe
Vivek K K <vivek.kk2024aiml@sece.ac.in>
2026-09-15 10:32:37 +0530

    Add solution: 1. Two Sum (cpp, v1)

---
+++ easy/1-two-sum/cpp_v1.cpp
@@ -0,0 +1,19 @@
+// Pushed: 2026-09-15 05:01:39 UTC
+// Difficulty: Easy
+// Runtime: 4 ms
+// Memory: 14.9 MB
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>mp;
+        for(int i=0; i<nums.size(); i++){
+            int req = target-nums[i];
+            if(mp.find(req) != mp.end()){
+                return {mp[req], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
