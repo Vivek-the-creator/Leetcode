4cb9823212c2b43e482560c49481ad4c78b1f006
Vivek K K <vivek.kk2024aiml@sece.ac.in>
2026-09-23 10:25:13 +0530

    Add solution: 1669. Merge In Between Linked Lists (cpp, v1)

---
+++ medium/1669-merge-in-between-linked-lists/cpp_v1.cpp
@@ -0,0 +1,37 @@
+// Pushed: 2026-09-23 04:55:10 UTC
+// Difficulty: Medium
+// Runtime: 183 ms
+// Memory: 99.5 MB
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list1;
+        ListNode* l2 = list2;
+        for(int i=0; i<a-1; i++){
+            temp1 = temp1->next;
+        }
+        for(int i=0; i<=b; i++){
+            temp2 = temp2->next;
+        }
+        temp1->next = l2;
+        while(l2->next != NULL){
+            l2 = l2->next;
+        }
+        
+        if(temp2 != NULL)
+            l2->next = temp2;
+        return list1;
+    }
+};
